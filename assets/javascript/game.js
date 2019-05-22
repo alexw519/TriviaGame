9ba4b8baed301f9114f1f510d5bf9f181b9a6619
Alex Wilson <alexw519@gmail.com>
2019-05-21 21:55:15 -0500

    Setting Up Questions Array

---
+++ assets/javascript/game.js
@@ -0,0 +1,147 @@
+//Creating Objects For The Questions
+var questions = 
+[
+    one = 
+    {
+        question: "Which Is Not A Unit Of Temperature?",
+        choices:
+        [
+            "Fahrenheit",
+            "Decible",
+            "Celsius",
+            "Kelvin"
+        ],
+        answerIndex: 2
+    },
+    two = 
+    {
+        question: "Which Is Not A Unit Of Temperature?",
+        choices:
+        [
+            "Fahrenheit",
+            "Decible",
+            "Celsius",
+            "Kelvin"
+        ],
+        answerIndex: 2
+    },
+    three = 
+    {
+        question: "Which Is Not A Unit Of Temperature?",
+        choices:
+        [
+            "Fahrenheit",
+            "Decible",
+            "Celsius",
+            "Kelvin"
+        ],
+        answerIndex: 2
+    },
+    four = 
+    {
+        question: "Which Is Not A Unit Of Temperature?",
+        choices:
+        [
+            "Fahrenheit",
+            "Decible",
+            "Celsius",
+            "Kelvin"
+        ],
+        answerIndex: 2
+    },
+    five = 
+    {
+        question: "Which Is Not A Unit Of Temperature?",
+        choices:
+        [
+            "Fahrenheit",
+            "Decible",
+            "Celsius",
+            "Kelvin"
+        ],
+        answerIndex: 2
+    },
+    six = 
+    {
+        question: "Which Is Not A Unit Of Temperature?",
+        choices:
+        [
+            "Fahrenheit",
+            "Decible",
+            "Celsius",
+            "Kelvin"
+        ],
+        answerIndex: 2
+    },
+    seven = 
+    {
+        question: "Which Is Not A Unit Of Temperature?",
+        choices:
+        [
+            "Fahrenheit",
+            "Decible",
+            "Celsius",
+            "Kelvin"
+        ],
+        answerIndex: 2
+    },
+    eight = 
+    {
+        question: "Which Is Not A Unit Of Temperature?",
+        choices:
+        [
+            "Fahrenheit",
+            "Decible",
+            "Celsius",
+            "Kelvin"
+        ],
+        answerIndex: 2
+    },
+    nine = 
+    {
+        question: "Which Is Not A Unit Of Temperature?",
+        choices:
+        [
+            "Fahrenheit",
+            "Decible",
+            "Celsius",
+            "Kelvin"
+        ],
+        answerIndex: 2
+    },
+    ten = 
+    {
+        question: "Which Is Not A Unit Of Temperature?",
+        choices:
+        [
+            "Fahrenheit",
+            "Decible",
+            "Celsius",
+            "Kelvin"
+        ],
+        answerIndex: 2
+    }    
+]
+
+var correctAnswers = 0;
+var wrongAnswers = 0;
+var questionNumber = 0;
+
+//Resets All Of The Values So The User Can Play Again
+function reset()
+{
+    correctAnswers = 0;
+    wrongAnswers = 0;
+    questionNumber = 0;
+    //Hide The Reset Button
+}
+
+function showQuestion(questionArray)
+{
+    //questionArray.question
+    //questionArray.choices[0]
+    //questionArray.choices[1]
+    //questionArray.choices[2]
+    //questionArray.choices[3]
+}
+

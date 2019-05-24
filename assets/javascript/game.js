c2051e139de1bf9be189101cbdebf03dc216b53c
Alex Wilson <alexw519@gmail.com>
2019-05-23 20:14:55 -0500

    Fixed Loop To Go To The Next Question

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,13 +15,13 @@ var questions =
     },
     two = 
     {
-        question: "Which Is Not A Unit Of Temperature?",
+        question: "Which Fingernail Grows The Fastest?",
         choices:
         [
-            "Fahrenheit",
-            "Decible",
-            "Celsius",
-            "Kelvin"
+            "Index Finger",
+            "Thumb",
+            "Middle Finger",
+            "Pointer Finger"
         ],
         answerIndex: 2
     },
@@ -128,7 +128,9 @@ var wrongAnswers = 0;
 var currentQuestion = 0;
 var questionNumber = 0;
 var guess = 0;
-var rightAnswer = false;
+var isRight = false;
+var rightAnswer;
+var correctIndex;
 
 showQuestion(questions);
 
@@ -137,8 +139,15 @@ function reset()
 {
     correctAnswers = 0;
     wrongAnswers = 0;
+    currentQuestion = 0;
     questionNumber = 0;
+    guess = 0;
+    isRight = false;
+
     //Hide The Reset Button
+
+
+    showQuestion(questions);
 }
 
 function showQuestion(questionArray)
@@ -146,68 +155,86 @@ function showQuestion(questionArray)
     $("#questionDiv").text();
     $("#questionDiv").append("<h2>" + questionArray[questionNumber].question + "</h2>");
 
-    // for (i = 0; i < 4; i++)
     for (i = 0; i < questionArray[questionNumber].choices.length; i++)
     {
-        $("#answer" + i).append("<h3 class='answers'>" + questionArray[questionNumber].choices[i] + "</h3>");
+        $("#answer" + i).html("<h3 class='answers'>" + questionArray[questionNumber].choices[i] + "</h3>");
     }
     currentQuestion = questionNumber;
+    correctIndex = questionArray[questionNumber].answerIndex;
+    rightAnswer = questionArray[questionNumber].choices[correctIndex];
     questionNumber++;
 }
 
 function resultScreen(result)
 {
+    $(".answers").text("");
+ 
     if (result)
     {
         correctAnswers++;
-        alert("That was the correct answer");
+        $("#questionDiv").html("<h2>Correct! " + rightAnswer + " was the right answer!</h2>");
     }
     else
     {
         wrongAnswers++;
-        alert("That was the wrong answer");
+        $("#questionDiv").html("<h2>Sorry, the Correct Answer Was: " + rightAnswer + "</h2>");
     }
+
+    //Wait At This Screen For Some Time, And Then
+    // if (questionNumber < 10)
+    // {
+    //    showQuestion(questions);
+    // }
+    // else
+    // {
+    //     finalScreen();
+    // }
+}
+
+function finalScreen()
+{
+    //Show Amount Of Correct And Wrong Answers
+    //Show The Reset Button
 }
 
 
 $("#answer0").on("click", function()
 {
-    // alert("clicked");
     guess = 0;
     if (guess === questions[currentQuestion].answerIndex)
-        rightAnswer = true;
+        isRight = true;
     else
-        rightAnswer = false;
+        isRight = false;
     
-    resultScreen(rightAnswer);
+    resultScreen(isRight);
 })
 $("#answer1").on("click", function()
 {
     guess = 1;
     if (guess === questions[currentQuestion].answerIndex)
-        rightAnswer = true;
+        isRight = true;
     else
-        rightAnswer = false;
+        isRight = false;
 
-    resultScreen(rightAnswer);
+    resultScreen(isRight);
 })
 $("#answer2").on("click", function()
 {
     guess = 2;
     if (guess === questions[currentQuestion].answerIndex)
-        rightAnswer = true;
+        isRight = true;
     else
-        rightAnswer = false;
+        isRight = false;
 
-    resultScreen(rightAnswer);
+    resultScreen(isRight);
 })
 $("#answer3").on("click", function()
 {
     guess = 3;
     if (guess === questions[currentQuestion].answerIndex)
-        rightAnswer = true;
+        isRight = true;
     else
-        rightAnswer = false;
+        isRight = false;
 
-    resultScreen(rightAnswer);
+    resultScreen(isRight);
 })
